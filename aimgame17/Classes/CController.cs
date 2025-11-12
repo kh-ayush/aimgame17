be90fad6a805611da365a38f7c6bac912bae2887
mrbobrovskiy <bob2ar@yandex.ru>
2025-11-12 16:43:26 +0700

    начал делать класс

--- aimgame17/Classes/CController.cs
+++ aimgame17/Classes/CController.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace aimgame17.Classes
 {
-    internal class CController
-    {
-    }
+    
+        public class CController
+        {
+             private List<CObject> objects;
+             private double spawnRate;
+            private double time;
+            private Random rng;
+        
+            private double minLifetime;
+            private double maxLifetime;
+           
+            private double minSpriteSize;
+            private double maxSpriteSize;
+            private Size sceneSize; 
+            private double points;
+            public CController(double spawnRate, ulong startTime, Size sceneSize)
+            {
+                rng = new Random();
+                objects = new List<CObject>();
+                this.spawnRate = spawnRate;
+                time = startTime;
+                this.sceneSize = sceneSize;
+                points = 0;
+                minLifetime = 1;
+                maxLifetime = 5;
+
+                minSpriteSize = 10;
+                maxSpriteSize = 20;
+            }
+        }
 }
